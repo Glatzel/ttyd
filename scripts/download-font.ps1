0968a96d0a64295b404fea98c9ddd98ba21b475f
Glatzel <chenzelin9794@gmail.com>
2025-11-16 17:03:32 +0800

    f

--- scripts/download-font.ps1
+++ scripts/download-font.ps1
@@ -1,4 +1,12 @@
 $ROOT = git rev-parse --show-toplevel
 gh release download -R "ryanoasis/nerd-fonts" -p "CascadiaMono.zip" `
         -O  $ROOT/temp/font.zip --clobber
-7z x "$ROOT/temp/font.zip" "-o$ROOT/html/src/style/font"
+7z x "$ROOT/temp/font.zip" "-o$ROOT/temp/font"
+pixi install
+foreach ($f in "CaskaydiaMonoNerdFont-Regular", "CaskaydiaMonoNerdFont-Italic", "CaskaydiaMonoNerdFont-Bold", "CaskaydiaMonoNerdFont-BoldItalic") {
+        pixi run pyftsubset $ROOT/temp/font/$f.ttf `
+                --output-file=$ROOT/html/src/style/webfont/$f.woff2 `
+                --flavor=woff2 `
+                --layout-features='*' `
+                --glyphs='*'
+}
