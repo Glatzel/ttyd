dec9c1647d0744bb0d32a5559a3061dc04436a2f
Glatzel <chenzelin9794@gmail.com>
2025-11-16 13:41:54 +0800

    f

--- scripts/download-font.ps1
+++ scripts/download-font.ps1
@@ -1,4 +1,4 @@
 $ROOT = git rev-parse --show-toplevel
 gh release download -R "ryanoasis/nerd-fonts" -p "CascadiaMono.zip" `
         -O  $ROOT/temp/font.zip --clobber
-7z x "$ROOT/temp/font.zip" "-o$ROOT/html/src/style/font"
+7z x "$ROOT/temp/font.zip" "-o$ROOT/html/src/font"
